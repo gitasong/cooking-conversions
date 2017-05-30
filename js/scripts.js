67f2414c117b943b9a676a121de3845f739c1783
Michelle Poterek and Nicole Freed <michelle@>
2017-05-30 14:40:34 -0700

    create initial project directory with index.html, css, and js folders; add initial js scripts

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var gallonsToLiters = function(gals) {
+	var liters = gals * 3.78541;
+  return(alert(liters));
+};
+
+var cupsToTablespoons = function(cups) {
+	var tablespoons = cups * 16;
+  return(alert(tablespoons));
+};
+
+//gallonsToLiters(1);
+//gallonsToLiters(3);
+//gallonsToLiters(0.25);
+
+cupsToTablespoons(1);
+cupsToTablespoons(-3);
+cupsToTablespoons(0.25);
